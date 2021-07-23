19aea9cd34a2bfc0926e83062cff03e0cb8003a4
Boris <boriscamayo@gmail.com>
2021-07-22 23:49:15 -0500

    cambios

--- src/main/java/cr/microservicios/mantenimientos/respository/ClienteRepository.java
+++ src/main/java/cr/microservicios/mantenimientos/respository/ClienteRepository.java
@@ -3,7 +3,7 @@ package cr.microservicios.mantenimientos.respository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import edymar.microservicios.commons.entity.Cliente;
+import cr.microservicios.commons.entity.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
--- src/main/java/cr/microservicios/mantenimientos/services/ClienteService.java
+++ src/main/java/cr/microservicios/mantenimientos/services/ClienteService.java
@@ -1,7 +1,7 @@
 package cr.microservicios.mantenimientos.services;
 
 import cr.microservicios.common.services.CommonService;
-import edymar.microservicios.commons.entity.Cliente;
+import cr.microservicios.commons.entity.Cliente;
 
 public interface ClienteService extends CommonService<Cliente> {
 
