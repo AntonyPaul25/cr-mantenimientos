1ae68e40b918ebefefe1b92f05be2490a22274bb
Boris <boriscamayo@gmail.com>
2021-08-03 10:31:51 -0500

    disable oauth2

--- src/main/java/cr/microservicios/mantenimientos/controller/MensajesController.java
+++ src/main/java/cr/microservicios/mantenimientos/controller/MensajesController.java
@@ -1,7 +1,7 @@
 package cr.microservicios.mantenimientos.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
