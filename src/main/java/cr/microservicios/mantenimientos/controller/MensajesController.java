53be8ff6b9415287da542f75fdba523707c93d5c
Tonny <antony.vp.25@gmail.com>
2021-08-09 02:10:30 -0500

    prueba BUS

--- src/main/java/cr/microservicios/mantenimientos/controller/MensajesController.java
+++ src/main/java/cr/microservicios/mantenimientos/controller/MensajesController.java
@@ -1,9 +1,18 @@
 package cr.microservicios.mantenimientos.controller;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.net.ssl.SSLContext;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
@@ -12,14 +21,19 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 //import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -133,6 +147,58 @@ public class MensajesController {
 		ResponseEntity<Object> response = restTemplate.postForEntity(url, entity, Object.class);
 
 		// System.out.println(data.toString());
+		return ResponseEntity.ok("test ok");
+	}
+
+	@GetMapping("/rtg")
+	public ResponseEntity<?> pruebaRtg() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
+
+		TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+		SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy)
+				.build();
+
+		SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+
+		requestFactory.setHttpClient(httpClient);
+
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+		Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Format formatterMessage = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
+
+		String url = "https://dpiuat.grupoib.local:7020/ibk/uat/api/enterprise-loan/v1/loan-limit/rating";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		headers.setBasicAuth("uBseGenuat", "Ibk2018$");
+		headers.add("X-INT-Timestamp", formatter.format(new Date()));
+		headers.add("X-IBM-Client-Id", "a3a383ba-3d4d-4a35-a85e-0cd0f09eae26");
+		headers.add("X-INT-Service-Id", "ATM");
+		headers.add("X-INT-Net-Id", "IS");
+		headers.add("X-INT-Branch-Id", "100");
+		headers.add("X-INT-Consumer-Id", "ATM");
+		headers.add("X-INT-Message-Id", formatterMessage.format(new Date()));
+		headers.add("X-INT-User-Id", "BSE0000");
+		headers.add("X-INT-Device-Id", "0");
+		headers.add("APIm-Debug", "true");
+		headers.add("X-INT-Supervisor-Id", "SXFL0000");
+
+		Map<String, Object> bodymap = new HashMap<String, Object>();
+		bodymap.put("codeID", "0050002375");
+		bodymap.put("documentIdType", "1");
+		bodymap.put("documentIdNumber", "20517698131");
+
+		HttpEntity<Map<String, Object>> entity = new HttpEntity<>(bodymap, headers);
+		ResponseEntity<Object> response = restTemplate.postForEntity(url, entity, Object.class);
+
+		System.out.println(response.getBody().toString());
+
 		return ResponseEntity.ok("test ok");
 	}
 
